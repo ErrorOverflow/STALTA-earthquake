66e25bd5e722a081479eba939723588a3645d31d
ErrorOverflow <wangmulan.buaa@foxmail.com>
2020-10-17 14:33:00 +0800

    cal f1

--- stalta_pre.py
+++ stalta_pre.py
@@ -2,18 +2,17 @@ import pandas as pd
 import h5py
 import numpy as np
 import obspy
-from keras import backend as K
 import matplotlib.pyplot as plt
 import time
 import csv
 from obspy.signal.trigger import recursive_sta_lta, classic_sta_lta, trigger_onset, ar_pick
 
 config = {
-    scale = 6000,
-    sta_window = 30,
-    lta_window = 120,
-    on_trigger = 1.4,
-    off_trigger = 0.7
+    'scale': 6000,
+    'sta_window': 300,
+    'lta_window': 600,
+    'on_trigger': 1.2,
+    'off_trigger': 0.5
 }
 
 
@@ -27,39 +26,14 @@ def judge(x):
     except ValueError:
         return False
 
-
-def f1(y_true, y_pred):
-    def recall(y_true, y_pred):
-        'Recall metric. Only computes a batch-wise average of recall. Computes the recall, a metric for multi-label classification of how many relevant items are selected.'
-
-        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-        recall = true_positives / (possible_positives + K.epsilon())
-        return recall
-
-    def precision(y_true, y_pred):
-        'Precision metric. Only computes a batch-wise average of precision. Computes the precision, a metric for multi-label classification of how many selected items are relevant.'
-        tmp = y_true * y_pred
-        print(tmp)
-        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-        precision = true_positives / (predicted_positives + K.epsilon())
-        return precision
-
-    precision = precision(y_true, y_pred)
-    recall = recall(y_true, y_pred)
-    return 2 * ((precision * recall) / (precision + recall + K.epsilon()))
-
-
 class DataOperator():
     def __init__(self, hdfpath, csvpath, outpath):
         self.hdfpath = hdfpath
         self.csvpath = csvpath
-        self.csvfile = open(out_path, 'w')
-        data_writer_initial()
+        self.csvfile = open(out_path, 'a')
 
     def data_generator(self):
-        df = pd.read_csv(csvpath)
+        df = pd.read_csv(self.csvpath)
         print(f'total events in csv file: {len(df)}')
 
         df = df[((df.trace_category == 'earthquake_local') & (
@@ -68,126 +42,143 @@ class DataOperator():
         print(f'total events selected: {len(df)}')
 
         ev_list = df['trace_name'].to_list()
-        dtfl = h5py.File(hdfpath, 'r')
+        dtfl = h5py.File(self.hdfpath, 'r')
         return dtfl, ev_list
 
     def data_writer_initial(self):
         output_writer = csv.writer(
-            csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            self.csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         # output_writer.writerow(['network_code', 'ID', 'earthquake_distance_km', 'snr_db', 'trace_name', 'trace_category', 'trace_start_time', 'source_magnitude', 'p_arrival_sample', 'p_status', 'p_weight', 's_arrival_sample', 's_status',
-        #                         's_weight', 'receiver_type', 'number_of_detections', 'detection_probability', 'detection_uncertainty', 'P_pick', 'P_probability', 'P_uncertainty', 'P_error', 'S_pick', 'S_probability', 'S_uncertainty', 'S_error'])       
-        output_writer.writerow(['p_arrival_sample', 's_arrival_sample','conda_sample_time','P_pick', 'S_pick', 'coda_end_pick'])
-        csvfile.flush()
-    
-    def data_writer(self, p_pick, s_pick, end_time):
+        #                         's_weight', 'receiver_type', 'number_of_detections', 'detection_probability', 'detection_uncertainty', 'P_pick', 'P_probability', 'P_uncertainty', 'P_error', 'S_pick', 'S_probability', 'S_uncertainty', 'S_error'])
+        output_writer.writerow(['trace_name', 'p_arrival_sample', 's_arrival_sample',
+                                'coda_end_sample', 'P_pick', 'S_pick', 'coda_end_pick'])
+        self.csvfile.flush()
+
+    def data_writer(self, trace_name, p_arrival_sample, s_arrival_sample, conda_sample_time, p_pick, s_pick, end_time):
         output_writer = csv.writer(
-            csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        output_writer.writerow([p_pick, s_pick, end_time])
-        csvfile.flush()
+            self.csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        output_writer.writerow(
+            [trace_name, p_arrival_sample, s_arrival_sample, conda_sample_time, p_pick, s_pick, end_time])
+        self.csvfile.flush()
 
 
-def predict(dtfl, ev_list):
+def predict(dtfl, ev_list, dataOperator):
     for c, evi in enumerate(ev_list):
-        dataset = dtfl.get('data/'+str(evi))
-        data = np.array(dataset)
+        try:
+            if c % 1000 == 0:
+                print(c)
+            dataset = dtfl.get('data/'+str(evi))
+            data = np.array(dataset)
 
-        pre_E = trigger_onset(recursive_sta_lta(
-            data[:, 0], sta_window, lta_window), on_trigger, off_trigger)
-        pre_N = trigger_onset(recursive_sta_lta(
-            data[:, 1], sta_window, lta_window), on_trigger, off_trigger)
-        pre_Z = trigger_onset(recursive_sta_lta(
-            data[:, 2], sta_window, lta_window), on_trigger, off_trigger)
+            pre_E = trigger_onset(recursive_sta_lta(
+                data[:, 0], config['sta_window'], config['lta_window']), config['on_trigger'], config['off_trigger'])
+            pre_N = trigger_onset(recursive_sta_lta(
+                data[:, 1], config['sta_window'], config['lta_window']), config['on_trigger'], config['off_trigger'])
+            # pre_Z = trigger_onset(recursive_sta_lta(
+            #     data[:, 2], config['sta_window'], config['lta_window']), config['on_trigger'], config['off_trigger'])
 
-        N_end_time, E_end_time = 6000, 6000
-        if len(pre_E):
-            E_end_time = pre_E[-1][1]
+            N_end_time, E_end_time = 6000, 6000
+            if len(pre_E) == 0 and len(pre_N) == 0:
+                dataOperator.data_writer(dataset.attrs['trace_name'], dataset.attrs['p_arrival_sample'],
+                                         dataset.attrs['s_arrival_sample'], dataset.attrs['coda_end_sample'], -1, -1, -1)
+                continue
 
-        if len(pre_N):
-            N_end_time = pre_N[-1][1]
+            if len(pre_E):
+                E_end_time = pre_E[-1][1]
 
-        end_time = (E_end_time + N_end_time) / 2
+            if len(pre_N):
+                N_end_time = pre_N[-1][1]
 
-        p_pick, s_pick = ar_pick(data[:, 0], data[:, 1], data[:, 2], 100,
-                                 1.0, 20.0, 1.0, 0.1, 4.0, 1.0, 2, 8, 0.1, 0.2) * 100
+            end_time = (E_end_time + N_end_time) / 2
 
-        # y_true = [float(dataset.attrs['p_arrival_sample']),
-        #           float(dataset.attrs['s_arrival_sample']), float(dataset.attrs['coda_end_sample'][0][0])]
-        # y_pred = [p_pick, s_pick, end_time]
+            p_pick, s_pick = ar_pick(data[:, 0], data[:, 1], data[:, 2], 100,
+                                     1.0, 20.0, 1.0, 0.1, 4.0, 1.0, 2, 8, 0.1, 0.2)
 
-        # p_true = np.zeros(shape=(6000,))
-        # p_true[p_pick-20:p_pick+21] = 1
+            p_pick, s_pick = p_pick*100, s_pick*100
 
-        # a = np.array(y_true)
-        # b = np.array(y_pred)
-        # print(a * b)
-        # break
-    return p_pick, s_pick, end_time
+            # y_true = [float(dataset.attrs['p_arrival_sample']),
+            #           float(dataset.attrs['s_arrival_sample']), float(dataset.attrs['coda_end_sample'][0][0])]
+            # y_pred = [p_pick, s_pick, end_time]
+
+            # p_true = np.zeros(shape=(6000,))
+            # p_true[p_pick-20:p_pick+21] = 1
+
+            # a = np.array(y_true)
+            # b = np.array(y_pred)
+            # print(a * b)
+            # break
+            dataOperator.data_writer(dataset.attrs['trace_name'], dataset.attrs['p_arrival_sample'],
+                                     dataset.attrs['s_arrival_sample'], dataset.attrs['coda_end_sample'], int(p_pick), int(s_pick), int(end_time))
+        except:
+            continue
+    return
 
 
-def show_result(p_pick, s_pick, end_time, is_save=False, is_show=False):
-    fig = plt.figure()
-    ax = fig.add_subplot(311)
-    plt.plot(data[:, 0], 'k')
-    plt.rcParams["figure.figsize"] = (8, 5)
-    legend_properties = {'weight': 'bold'}
-    plt.tight_layout()
-    ymin, ymax = ax.get_ylim()
+# def show_result(p_pick, s_pick, end_time, is_save=False, is_show=False):
+#     fig = plt.figure()
+#     ax = fig.add_subplot(311)
+#     plt.plot(data[:, 0], 'k')
+#     plt.rcParams["figure.figsize"] = (8, 5)
+#     legend_properties = {'weight': 'bold'}
+#     plt.tight_layout()
+#     ymin, ymax = ax.get_ylim()
 
-    pl = plt.vlines(p_pick, ymin,
-                    ymax, color='b', linewidth=2, label='P-arrival')
-    sl = plt.vlines(s_pick, ymin,
-                    ymax, color='r', linewidth=2, label='S-arrival')
-    cl = plt.vlines(end_time, ymin,
-                    ymax, color='aqua', linewidth=2, label='Coda End')
-    plt.legend(handles=[pl, sl, cl], loc='upper right',
-               borderaxespad=0., prop=legend_properties)
-    plt.ylabel('Amplitude counts', fontsize=12)
-    ax.set_xticklabels([])
+#     pl = plt.vlines(p_pick, ymin,
+#                     ymax, color='b', linewidth=2, label='P-arrival')
+#     sl = plt.vlines(s_pick, ymin,
+#                     ymax, color='r', linewidth=2, label='S-arrival')
+#     cl = plt.vlines(end_time, ymin,
+#                     ymax, color='aqua', linewidth=2, label='Coda End')
+#     plt.legend(handles=[pl, sl, cl], loc='upper right',
+#                borderaxespad=0., prop=legend_properties)
+#     plt.ylabel('Amplitude counts', fontsize=12)
+#     ax.set_xticklabels([])
 
-    ax = fig.add_subplot(312)
-    plt.plot(data[:, 1], 'k')
-    plt.rcParams["figure.figsize"] = (8, 5)
-    legend_properties = {'weight': 'bold'}
-    plt.tight_layout()
-    ymin, ymax = ax.get_ylim()
-    pl = plt.vlines(p_pick, ymin,
-                    ymax, color='b', linewidth=2, label='P-arrival')
-    sl = plt.vlines(s_pick, ymin,
-                    ymax, color='r', linewidth=2, label='S-arrival')
-    cl = plt.vlines(end_time, ymin,
-                    ymax, color='aqua', linewidth=2, label='Coda End')
-    plt.legend(handles=[pl, sl, cl], loc='upper right',
-               borderaxespad=0., prop=legend_properties)
-    plt.ylabel('Amplitude counts', fontsize=12)
-    ax.set_xticklabels([])
+#     ax = fig.add_subplot(312)
+#     plt.plot(data[:, 1], 'k')
+#     plt.rcParams["figure.figsize"] = (8, 5)
+#     legend_properties = {'weight': 'bold'}
+#     plt.tight_layout()
+#     ymin, ymax = ax.get_ylim()
+#     pl = plt.vlines(p_pick, ymin,
+#                     ymax, color='b', linewidth=2, label='P-arrival')
+#     sl = plt.vlines(s_pick, ymin,
+#                     ymax, color='r', linewidth=2, label='S-arrival')
+#     cl = plt.vlines(end_time, ymin,
+#                     ymax, color='aqua', linewidth=2, label='Coda End')
+#     plt.legend(handles=[pl, sl, cl], loc='upper right',
+#                borderaxespad=0., prop=legend_properties)
+#     plt.ylabel('Amplitude counts', fontsize=12)
+#     ax.set_xticklabels([])
 
-    ax = fig.add_subplot(313)
-    plt.plot(data[:, 2], 'k')
-    plt.rcParams["figure.figsize"] = (8, 5)
-    legend_properties = {'weight': 'bold'}
-    plt.tight_layout()
-    ymin, ymax = ax.get_ylim()
-    pl = plt.vlines(p_pick, ymin,
-                    ymax, color='b', linewidth=2, label='P-arrival')
-    sl = plt.vlines(s_pick, ymin,
-                    ymax, color='r', linewidth=2, label='S-arrival')
-    cl = plt.vlines(end_time, ymin,
-                    ymax, color='aqua', linewidth=2, label='Coda End')
-    plt.legend(handles=[pl, sl, cl], loc='upper right',
-               borderaxespad=0., prop=legend_properties)
-    plt.ylabel('Amplitude counts', fontsize=12)
-    ax.set_xticklabels([])
-    if is_show:
-        plt.show()
-    if is_save:
-        plt.savefig("./predict_pic/merged/"+str(c)+".png")
-    plt.close()
+#     ax = fig.add_subplot(313)
+#     plt.plot(data[:, 2], 'k')
+#     plt.rcParams["figure.figsize"] = (8, 5)
+#     legend_properties = {'weight': 'bold'}
+#     plt.tight_layout()
+#     ymin, ymax = ax.get_ylim()
+#     pl = plt.vlines(p_pick, ymin,
+#                     ymax, color='b', linewidth=2, label='P-arrival')
+#     sl = plt.vlines(s_pick, ymin,
+#                     ymax, color='r', linewidth=2, label='S-arrival')
+#     cl = plt.vlines(end_time, ymin,
+#                     ymax, color='aqua', linewidth=2, label='Coda End')
+#     plt.legend(handles=[pl, sl, cl], loc='upper right',
+#                borderaxespad=0., prop=legend_properties)
+#     plt.ylabel('Amplitude counts', fontsize=12)
+#     ax.set_xticklabels([])
+#     if is_show:
+#         plt.show()
+#     if is_save:
+#         plt.savefig("./predict_pic/merged/"+str(c)+".png")
+#     plt.close()
 
 
 def main(hdfpath, csvpath, out_path):
     dataOperator = DataOperator(hdfpath, csvpath, out_path)
+    dataOperator.data_writer_initial()
     dtfl, ev_list = dataOperator.data_generator()
-    p_pick, s_pick, end_time = predict(dtfl, ev_list, dataOperator)
+    predict(dtfl, ev_list, dataOperator)
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,14 +1,73 @@
 import numpy as np
 import pandas as pd
 
-source = pd.read_csv("D:\\flushSTEAD\\merged.csv")
-source.drop(['network_code','receiver_code','receiver_type','receiver_latitude','receiver_longitude',
-                                 'receiver_elevation_m','p_status','p_weight','p_travel_sec',
-                                 's_status','s_weight',
-                                 'source_id','source_origin_time','source_origin_uncertainty_sec',
-                                 'source_latitude','source_longitude','source_error_sec',
-                                 'source_gap_deg','source_horizontal_uncertainty_km', 'source_depth_uncertainty_km',
-                                 'source_magnitude', 'source_magnitude_type', 'source_magnitude_author','source_mechanism_strike_dip_rake',
-                                 'source_distance_deg', 'back_azimuth_deg', 'snr_db',
-                                 'trace_start_time'], axis = 1)
-source.to_csv("D:\\flushSTEAD\\predict.csv")
+def create_csv():
+    source = pd.read_csv("/media/wml/新加卷/flushSTEAD/merged.csv")
+    source = source.drop(['network_code','receiver_code','receiver_type','receiver_latitude','receiver_longitude',
+                                    'receiver_elevation_m','p_status','p_weight','p_travel_sec',
+                                    's_status','s_weight',
+                                    'source_id','source_origin_time','source_origin_uncertainty_sec',
+                                    'source_latitude','source_longitude','source_error_sec',
+                                    'source_gap_deg','source_horizontal_uncertainty_km', 'source_depth_uncertainty_km',
+                                    'source_magnitude', 'source_magnitude_type', 'source_magnitude_author','source_mechanism_strike_dip_rake',
+                                    'source_distance_deg', 'back_azimuth_deg', 'snr_db',
+                                    'trace_start_time'], axis = 1)
+    source.to_csv("/media/wml/新加卷/flushSTEAD/predict.csv")
+
+def f1(y_true, y_pred):
+    def recall(y_true, y_pred):
+        'Recall metric. Only computes a batch-wise average of recall. Computes the recall, a metric for multi-label classification of how many relevant items are selected.'
+
+        true_positives = np.sum(np.round(np.clip(y_true * y_pred, 0, 1)))
+        possible_positives = np.sum(np.round(np.clip(y_true, 0, 1)))
+        recall = true_positives / (possible_positives + 1e-07)
+        return recall
+
+    def precision(y_true, y_pred):
+        'Precision metric. Only computes a batch-wise average of precision. Computes the precision, a metric for multi-label classification of how many selected items are relevant.'
+        true_positives = np.sum(np.round(np.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = np.sum(np.round(np.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + 1e-07)
+        return precision
+
+    precision = precision(y_true, y_pred)
+    recall = recall(y_true, y_pred)
+    return 2 * ((precision * recall) / (precision + recall + 1e-07))
+
+def array_generate(point):
+    array = np.zeros(shape = (6000,))
+    if point < 50:
+        array[0 : point+51] = 1
+    elif point > 6000 - 50:
+        array[point-50 : 6000] = 1 
+    else:
+        array[point-50 : point+51] = 1 
+    return array
+
+def calcu():
+    df = pd.read_csv("/media/wml/新加卷/flushSTEAD/result1602857324.2880619.csv")
+    p_f1_total = 0
+    s_f1_total = 0
+    end_f1_total = 0
+    for index in df.index:
+        p_array_t = array_generate(int(df.loc[index].values[1]))
+        p_array_p = array_generate(df.loc[index].values[4])
+        s_array_t = array_generate(int(df.loc[index].values[2]))
+        s_array_p = array_generate(df.loc[index].values[5])
+
+        end_array_t = np.zeros(shape = (6000,))
+        end_array_t[int(df.loc[index].values[1]) : int(df.loc[index].values[3][3:-3])] = 1
+        end_array_p = np.zeros(shape = (6000,))
+        if df.loc[index].values[6] >= df.loc[index].values[4]:
+            end_array_p[int(df.loc[index].values[4]) : int(df.loc[index].values[6])] = 1
+
+        p_f1_total += f1(p_array_t, p_array_p)
+        s_f1_total += f1(s_array_t, s_array_p)
+        end_f1_total += f1(end_array_t, end_array_p)
+
+        if index % 100 == 0:
+            print(f'round {index} : p_f1_score {p_f1_total/index}, s_f1_score {s_f1_total/index}, end_f1_score {end_f1_total/index}')
+    
+
+if __name__ == "__main__":
+    calcu()
